a6134e4a308c2a46f805cc127823941bbb254499
Yemyja <yemyja@gmail.com>
2023-08-24 20:20:15 -0600

    aa

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,11 +59,10 @@
 // export default Home;
 
 //_____________________
-import React, { useState } from "react";
+import React from "react";
 
 import { Link } from "react-router-dom"; // Import Link from React Router
 import "bootstrap/dist/css/bootstrap.min.css"; // Importa el CSS de Bootstrap
-
 import "./Home.css";
 
 function Home() {
